ac8b52f4ed04307a2b74cc1b5ad5fd507b58729a
Anki1229 <70660319+Anki1229@users.noreply.github.com>
2021-03-04 16:44:21 +0530

    Update ec2.tf
    updated ami

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
   }
 resource "aws_instance" "web" {
-  ami           = "ami-01ad94fdf8150776c"
+  ami           = "ami-0eeb03e72075b9bcc"
   instance_type = "t2.micro"
 
   tags = {
